dd53c6a0c87e5edd912985860c441b9fcb2d85b9
Andrey Sergeevich <kaymantex68@yandex.ru>
2021-02-06 15:28:50 +0300

    11 lessons FIREBASE -> 12

---
+++ client/src/components/nav/Header.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  UserOutlined,
+  UserAddOutlined,
+  EyeOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+import {Link} from 'react-router-dom'
+
+const { SubMenu, Item } = Menu; //Menu.SubMenu
+
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
+  const handleClick = (e) => {
+    // console.log(e.key)
+    setCurrent(e.key);
+  };
+  return (
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<EyeOutlined />}>
+       <Link to="/">Home</Link>
+      </Item>
+
+      <Item
+        key="register"
+        icon={<UserAddOutlined />}
+        className="float-right"
+      >
+        <Link to="/register">Register</Link>
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
+      <Link to="/login">Login</Link>
+      </Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+export default Header;
---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM4T-IzMYxL-4J3HZAV2-6z2GG4b8kW1U",
+  authDomain: "ecommerce-99300.firebaseapp.com",
+  projectId: "ecommerce-99300",
+  storageBucket: "ecommerce-99300.appspot.com",
+  messagingSenderId: "230899564767",
+  appId: "1:230899564767:web:ac6814e2156746e1a2296a",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleAuthProvider = firebase.auth.googleAuthProvider()
