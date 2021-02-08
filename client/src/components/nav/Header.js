4d25c0c33c1fac516e2d0ce2220f81db86ecd65a
Andrey Sergeevich <kaymantex68@yandex.ru>
2021-02-08 17:27:26 +0300

    23 lessons -> 24 Добавил имя пользователя при логировании  и некоторые стили

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -9,7 +9,7 @@ import {
 } from "@ant-design/icons";
 import firebase from "firebase";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom'
 
 const { SubMenu, Item } = Menu; //Menu.SubMenu
@@ -17,10 +17,11 @@ const { SubMenu, Item } = Menu; //Menu.SubMenu
 const Header = () => {
     const [current, setCurrent] = useState("home");
     const dispatch = useDispatch();
+    let { user } = useSelector((state) => ({ ...state }))
     const history = useHistory()
     const handleClick = (e) => {
         // console.log(e.key)
-        setCurrent(e.key);
+        setCurrent(e.key)
     };
 
     const logout = () => {
@@ -34,24 +35,27 @@ const Header = () => {
     return (
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
             <Item key="home" icon={<EyeOutlined />}>
-                <Link to="/">Home</Link>
+                <Link to="/">Home </Link>
             </Item>
 
-            <Item key="register" icon={<UserAddOutlined />} className="float-right">
-                <Link to="/register">Register</Link>
-            </Item>
+            {!user &&
+                <Item key="register" icon={<UserAddOutlined />} className="float-right">
+                    <Link to="/register">Register</Link>
+                </Item>}
 
-            <Item key="login" icon={<UserOutlined />} className="float-right">
-                <Link to="/login">Login</Link>
-            </Item>
+            {!user &&
+                <Item key="login" icon={<UserOutlined />} className="float-right">
+                    <Link to="/login">Login</Link>
+                </Item>}
 
-            <SubMenu icon={<SettingOutlined />} title="Username">
-                <Item key="setting:1">Option 1</Item>
-                <Item key="setting:2">Option 2</Item>
-                <Item icon={<LogoutOutlined />} onClick={logout}>
-                    logout
-        </Item>
-            </SubMenu>
+            {user &&
+                <SubMenu icon={<SettingOutlined />} className="float-right" title={user.email && user.email.split('@')[0]}>
+                    <Item key="setting:1">Option 1</Item>
+                    <Item key="setting:2">Option 2</Item>
+                    <Item icon={<LogoutOutlined />} onClick={logout}>
+                        logout
+                    </Item>
+                </SubMenu>}
         </Menu>
     );
 };
