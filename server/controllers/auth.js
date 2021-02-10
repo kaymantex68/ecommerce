b93811bf90770d073786e40cae5ecc73cfcb42c7
Andrey Sergeevich <kaymantex68@yandex.ru>
2021-02-10 17:35:47 +0300

    37 lesson  -> 38  User create user update first write in database (mongo db)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,25 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-        data: "hey you hit finish API",
-    })
-}
+const User = require("../models/user");
+
+exports.createOrUpdateUser = async (req, res) => {
+    const { name, picture, email } = req.user;
+
+    // update user in database
+    const user = await User.findOneAndUpdate(
+        { email },
+        { name, picture },
+        { new: true }
+    );
+
+    if (user) {
+        console.log('USER UPDATED', user)
+        res.json(user);
+    } else {
+        const newUser = await new User({
+            email,
+            name,
+            picture,
+        }).save();
+        console.log('USER CREATED', newUser)
+        res.json(newUser)
+    }
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,19 @@
 const admin = require('../firebase')
 
-exports.authCheck= (req, res,next) => {
-    console.log(req.headers)
-    next()
+exports.authCheck= async (req, res,next) => {
+    // console.log(req.headers)
+    try{
+        const firebaseUser = await admin
+        .auth()
+        .verifyIdToken(req.headers.authtoken)
+        // console.log('FIREBASE USER IS AUTHCHECK', firebaseUser)
+        req.user = firebaseUser
+        next()
+    }catch(err){
+        console.log(err)
+        res.status(401).json({
+            message: 'Invalid or expired token'
+        })
+    }
+    
 }
