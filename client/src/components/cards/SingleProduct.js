58fdd7d437e040e6d7392ac433f4f822748a96c3
Andrey Sergeevich <kaymantex68@yandex.ru>
2021-02-20 15:13:11 +0300

    124 lesson -> 125 designe single product card

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,37 +1,53 @@
-import React from 'react'
-import { Card } from 'antd'
-import { Link } from 'react-router-dom'
-import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons'
-const {Meta} = Card 
+import React from "react";
+import { Card } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import cat from "../../images/IMG_7923.JPG";
+const { Meta } = Card;
 
-const SingleProduct = ({product}) => {
-    const { title, description, images, slug } = product
-    return(
+const SingleProduct = ({ product }) => {
+    const { title, description, images, slug } = product;
+    return (
         <>
             <div className="col-md-7">
-                image carousel
+                {images && images.length ? (
+                    <Carousel shoeArrows={true} autoPlay infinitelLoop>
+                        {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+                    </Carousel>
+                ) : (
+                        <Card
+                            cover={
+                                <img
+                                    src={cat}
+                                    className="mb-3 card-image"
+                                />
+                            }
+                        ></Card>
+                    )}
             </div>
             <div className="col-md-5">
                 <Card
                     actions={[
                         <>
-                            <ShoppingCartOutlined className="class-succes"/> <br/>
-                            Add to cart 
-                        </>,
+                            <ShoppingCartOutlined className="class-succes" /> <br />
+              Add to cart
+            </>,
                         <Link to="/">
-                            <HeartOutlined className="text-info"/> <br/>
-                            Add to Wishlist
-                        </Link>,
+                            <HeartOutlined className="text-info" /> <br />
+              Add to Wishlist
+            </Link>,
                     ]}
                 >
                     <Meta title={title} description={description} />
                     <p>
                         /price/category/subs/shiping/color/brand/quantity available/sold
-                        </p>
+          </p>
                 </Card>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default SingleProduct
+export default SingleProduct;
