9662243063ad5a8e49ae21b04cf46fc274fe3450
Home <kaymantex68@yandex.ru>
2021-02-20 21:20:17 +0300

    125 lesson -> 126 single card

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -4,50 +4,44 @@ import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import ProductListItems from '../cards/ProductListItems'
 import cat from "../../images/IMG_7923.JPG";
-const { Meta } = Card;
+
 
 const SingleProduct = ({ product }) => {
-    const { title, description, images, slug } = product;
-    return (
-        <>
-            <div className="col-md-7">
-                {images && images.length ? (
-                    <Carousel shoeArrows={true} autoPlay infinitelLoop>
-                        {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
-                    </Carousel>
-                ) : (
-                        <Card
-                            cover={
-                                <img
-                                    src={cat}
-                                    className="mb-3 card-image"
-                                />
-                            }
-                        ></Card>
-                    )}
-            </div>
-            <div className="col-md-5">
-                <Card
-                    actions={[
-                        <>
-                            <ShoppingCartOutlined className="class-succes" /> <br />
+  const { title, images} = product;
+  return (
+    <>
+      <div className="col-md-7">
+
+        {images && images.length ? (
+          <Carousel shoeArrows={true} autoPlay infinitelLoop>
+            {images &&
+              images.map((i) => <img src={i.url} key={i.public_id} />)}
+          </Carousel>
+        ) : (
+          <Card cover={<img src={cat} className="mb-3 card-image" />}> </Card>
+        )}
+      </div>
+      <div className="col-md-5">
+        <h1 className="bg-info p-3"> {title} </h1>
+        <Card
+          actions={[
+            <>
+              <ShoppingCartOutlined className="class-succes" /> <br />
               Add to cart
             </>,
-                        <Link to="/">
-                            <HeartOutlined className="text-info" /> <br />
+            <Link to="/">
+              <HeartOutlined className="text-info" /> <br />
               Add to Wishlist
             </Link>,
-                    ]}
-                >
-                    <Meta title={title} description={description} />
-                    <p>
-                        /price/category/subs/shiping/color/brand/quantity available/sold
-          </p>
-                </Card>
-            </div>
-        </>
-    );
+          ]}
+        >
+          <ProductListItems product={product}/>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default SingleProduct;
