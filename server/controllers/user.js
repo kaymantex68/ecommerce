3a5ca3d773b124234fdd978828d9428990c8b4d8
Home <kaymantex68@yandex.ru>
2021-03-09 14:37:33 +0300

    183 lesson -> 184 but not shure

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -47,7 +47,7 @@ exports.userCart = async (req, res) => {
 }
 
 exports.getUserCart = async (req, res) => {
-    console.log('we here')
+    // console.log('we here')
     const user = await User.findOne({ email: req.user.email }).exec()
     let cart = await Cart.findOne({ orderBy: user._id })
         .populate('products.product', '_id title price totalAfterDiscount')
@@ -55,4 +55,16 @@ exports.getUserCart = async (req, res) => {
     const { products, cartTotal, totalAfterDiscount } = cart
     console.log('userCart----------->', cart)
     res.json({ products, cartTotal, totalAfterDiscount })
+}
+
+
+exports.emptyCart = async (req, res) => {
+    const user = await User.findOne({ email: req.user.email }).exec()
+    const cart = await Cart.findOneAndRemove({ orderBy: user._id }).exec()
+    res.json(cart)
+}
+
+exports.saveAddress = async (req, res) => {
+    const userAddress= await User.findOneAndUpdate({email: req.user.email},{address: req.body.address}).exec()
+    res.json({ok: true})
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,16 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 // middlewares
-const { authCheck,adminCheck }=require('../middlewares/auth')
+const { authCheck } = require('../middlewares/auth')
 // controllers
-const { userCart, getUserCart }=require('../controllers/user')
+const { userCart, getUserCart, emptyCart, saveAddress } = require('../controllers/user')
 
-router.post('/user/cart',authCheck, userCart);
+router.post('/user/cart', authCheck, userCart);
 router.get('/user/cart', authCheck, getUserCart)
-// router.get('/user', (req, res) => {
-//     res.json({
-//         data: "user API endpoint",
-//     });
-// })
+router.put('/user/cart', authCheck, emptyCart)
+router.post('/user/address', authCheck, saveAddress)
 
 module.exports = router
